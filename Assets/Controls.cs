1e5d64eecd9f1fd2ab23d87c36691e8f64e7cf72
Andrew Park <andrew.park708@gmail.com>
2018-06-03 21:27:24 -0400

    Unity First Upload

---
+++ Assets/Controls.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controls : MonoBehaviour {
+
+    private Rigidbody player;
+    public float speed = 1f;
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("Hello");
+
+        player = GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey("w"))
+        {
+            player.position = player.position + new Vector3(0, 0, speed);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            player.position = player.position + new Vector3(-speed, 0, 0);
+        }
+
+        if (Input.GetKey("s"))
+        {
+            player.position = player.position + new Vector3(0, 0, -speed);
+        }
+
+        if (Input.GetKey("d"))
+        {
+            player.position = player.position + new Vector3(speed, 0, 0);
+        }
+
+    }
+}
